3041c45bbd862ca4d5c206948dbf16df5bed92e8
rbouchnick <rbouchnick@isracard.co.il>
2022-09-28 00:17:29 +0300

    pushCode

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './redux/store';
 import { Provider } from 'react-redux'
-
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+import { HomePage, Favorite } from './screens'
+// import { Header } from './components';
+// import "react-loader-spinner/dist/loader/css/react-spinner-load
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <div className="App">
+          {/* <Header /> */}
+          {/* <ToastContainer position='top-center' /> */}
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/:city" element={<HomePage />} />
+            <Route path="/favorite" element={<Favorite />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
