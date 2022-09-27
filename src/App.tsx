b6b6664eb45db6cfabec4c173035138471139189
rbouchnick <rbouchnick@isracard.co.il>
2022-09-27 23:54:14 +0300

    pushCode

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { HomePage, Favorite } from './screens'
+import { Header } from './components';
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <ToastContainer position='top-center' />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:city" element={<HomePage />} />
+          <Route path="/favorite" element={<Favorite />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
