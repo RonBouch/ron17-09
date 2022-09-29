ea2e89a76bd8ca56afcbb150ad968b0999bfb944
rbouchnick <rbouchnick@isracard.co.il>
2022-09-29 15:45:18 +0300

    pushCode

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './redux/store';
 import { Provider } from 'react-redux'
-import './App.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { HomePage, Favorite } from './screens'
-// import { Header } from './components';
-// import "react-loader-spinner/dist/loader/css/react-spinner-load
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <React.Fragment>
     <Provider store={store}>
-      <HashRouter>
-        <div className="App">
-          {/* <Header /> */}
-          {/* <ToastContainer position='top-center' /> */}
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            {/* <Route path="/:city" element={<HomePage />} /> */}
-            {/* <Route path="/favorite" element={<Favorite />} /> */}
-          </Routes>
-        </div>
-      </HashRouter>
+      <App />
     </Provider>
-  </React.StrictMode>
+  </React.Fragment>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
